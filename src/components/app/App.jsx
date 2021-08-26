93e72c7f95be9d8b57e4148b6249f796a9478c5c
Andrey <demyanov23@gmail.com>
2021-08-26 11:33:59 +0300

    html layout + css [main page]

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +14,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
